ee5b52a40ad6ce810c48ec9646042c94c23be808
mwakazi-cio <johnson.mwakazi@cioafrica.co>
2025-03-21 15:38:01 +0300

    update port from 8000 to 5500

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ connectToDB();
 // all routes
 
 // start the express server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
