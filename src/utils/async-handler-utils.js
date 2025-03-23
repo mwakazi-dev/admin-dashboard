e6f9dc1ebffd3c17028feadcf1c0d19abfef0a79
mwakazi-cio <johnson.mwakazi@cioafrica.co>
2025-03-23 22:34:12 +0300

    chore: prepare for deployment

--- src/utils/async-handler-utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var asyncHandler = function (fn) {
-    return function (req, res, next) {
-        return Promise.resolve(fn(req, res, next)).catch(next);
-    };
-};
-exports.default = asyncHandler;
--- src/utils/otp-utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.otpExpiresDate = exports.generateOTP = void 0;
-var generateOTP = function () {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-};
-exports.generateOTP = generateOTP;
-// otp expires in 10 minutes
-exports.otpExpiresDate = new Date(Date.now() + 10 * 60 * 1000);
